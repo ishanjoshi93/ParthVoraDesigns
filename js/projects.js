269e70525dcfd08b244f22a08421aef19ab73bee
Ishan Joshi <ishanjoshi93@gmail.com>
2020-06-25 21:57:28 +0530

    first commit

---
+++ js/projects.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var thumbnail = document.getElementsByClassName("thumbnail");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < thumbnail.length; i++) {
+      thumbnail[i].className = thumbnail[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  thumbnail.className += " active";
+
+}
